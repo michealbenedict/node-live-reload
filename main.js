f0f73616740165313647487b8e53a6bab8982928
Micheal <micheal@visionmasterdesigns.com>
2012-09-25 20:18:48 -0400

    i like semicolons;

--- main.js
+++ main.js
@@ -51,8 +51,8 @@ watchr.watch({
 
 wss.on('connection', function ( ws ) {
   emitter.once('reload', function () {
-    ws.send(JSON.stringify({r: Date.now().toString()}), function (e) {
-      if ( !e ) { return }
+    ws.send(JSON.stringify({r: Date.now().toString()}), function ( e ) {
+      if ( !e ) { return; }
       console.log(e);
     });
   });
